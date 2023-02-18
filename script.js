ce978864ccbdce9336ff4c22287af14ab3e71078
C00MAR <moureau.marc78@gmail.com>
2023-02-18 15:02:09 +0100

    :sparkles: Choose Which Crypto in Input :sparkles:

--- script.js
+++ script.js
@@ -2,11 +2,10 @@ var inputdate = document.getElementById("date_input")
 inputdate.setAttribute("max", new Date().toLocaleDateString('fr-ca'))
 
 var canvas = document.getElementById('crypto_chart').getContext('2d');
-
-var labelsSelectDate = []
-
+var currency = "bitcoin";
+var graph = 0
+var labelsSelectDate = [];
 var labelsHours = [];
-
 var labelsDays = [];
 
 function LabelsDays() {
@@ -64,13 +63,12 @@ var chartData = {
     }
     , {
         label: 'Ethereum',
-        data: [800, 1200, 900, 1000, 700, 600, 800, 1200, 900, 1150, 750],
+        data: [],
         borderColor: '#000000',
         fill: true,
         tension: 0.25
-    }
-    ]
-}
+    }]
+};
 
 var chartOptions = {
     responsive: true,
@@ -103,24 +101,24 @@ var chartOptions = {
     }
 };
 
-function DataUpdate(date) {
-    chartData.datasets[0].data = [];
-    fetch('https://api.coingecko.com/api/v3/coins/bitcoin/history?date='+date)
+function DataUpdate(date, graph = 0, currency = 'bitcoin') {
+    chartData.datasets[graph].data = [];
+    fetch('https://api.coingecko.com/api/v3/coins/' + currency + '/history?date=' + date)
         .then(response => response.json())
         .then(data => {
-            const prices = data.market_data.current_price.eur
+            const prices = data.market_data.current_price.eur;
 
-            chartData.datasets[0].data.push(prices);
+            chartData.datasets[graph].data.push(prices);
 
             chart.data = chartData;
             chart.update();
         })
         .catch(error => console.error(error));
-    }
+}
 
 
 function DayPerDay() {
-    chartData.labels = labelsDays
+    chartData.labels = labelsDays;
     chartOptions.scales.x.min = labelsDays[0];
     chartOptions.scales.x.ticks.stepSize = 1;
     document.getElementById("Hour").classList.remove("active");
@@ -134,8 +132,6 @@ function DayPerDay() {
         const date2 = parts.reverse().join('-');
         DataUpdate(date2)
     }
-    console.log(chartData.datasets[0].data)
-    chart.update();
 }
 
 function DayThirdDay() {
@@ -153,8 +149,6 @@ function DayThirdDay() {
         const date2 = parts.reverse().join('-');
         DataUpdate(date2)
     }
-    console.log(chartData.datasets[0].data)
-    chart.update();
 }
 
 function SearchDate(){
@@ -170,14 +164,91 @@ function SearchDate(){
     chartOptions.scales.x.min = date2;
     chartOptions.scales.x.max = date2;
     labelsSelectDate.push(document.getElementById("date_input").value)
-    DataUpdate(date2)
-    console.log(chartData)
-    chart.update();
+    DataUpdate(date2, 0, currency)
+    console.log(chartData.datasets[0].data)
 }
 
 function SelectDate() {
     var divinput = document.getElementById("date");
     divinput.classList.toggle("dateshow")
+}
+
+function SelectCurrencyOne() {
+    var Input_Currency_One = document.getElementById("currency_one");
+    currency = Input_Currency_One.value
+    if (document.getElementById("DayThird").classList == "active") {
+        chartData.labels = labels3Days
+        chartOptions.scales.x.min = labels3Days[0];
+        chartOptions.scales.x.ticks.stepSize = 3;
+        document.getElementById("Hour").classList.remove("active");
+        document.getElementById("Day").classList.remove("active");
+        document.getElementById("Week").classList.remove("active");
+        document.getElementById("Month").classList.remove("active");
+        document.getElementById("valid_calendar").classList.remove("active");
+        document.getElementById("DayThird").classList.add("active");
+        for (var i = 0; i < 10; i++) {
+            const parts = labels3Days[i].split('-');
+            const date2 = parts.reverse().join('-');
+            DataUpdate(date2, 0, currency)
+        }
+        console.log(chartData.datasets[graph].data)
+    }    
+    else {
+        chartData.labels = labelsDays;
+        chartOptions.scales.x.min = labelsDays[0];
+        chartOptions.scales.x.ticks.stepSize = 1;
+        document.getElementById("Hour").classList.remove("active");
+        document.getElementById("DayThird").classList.remove("active");
+        document.getElementById("Week").classList.remove("active");
+        document.getElementById("Month").classList.remove("active");
+        document.getElementById("valid_calendar").classList.remove("active");
+        document.getElementById("Day").classList.add("active");
+        for (var i = 0; i < 10; i++) {
+            const parts = labelsDays[i].split('-');
+            const date2 = parts.reverse().join('-');
+            DataUpdate(date2, 0, currency)
+        }
+        console.log(chartData.datasets[0].data)
+    }
+}
+
+function SelectCurrencyTwo() {
+    var Input_Currency_Two = document.getElementById("currency_two");
+    currency = Input_Currency_Two.value
+    if (document.getElementById("DayThird").classList == "active") {
+        chartData.labels = labels3Days
+        chartOptions.scales.x.min = labels3Days[0];
+        chartOptions.scales.x.ticks.stepSize = 3;
+        document.getElementById("Hour").classList.remove("active");
+        document.getElementById("Day").classList.remove("active");
+        document.getElementById("Week").classList.remove("active");
+        document.getElementById("Month").classList.remove("active");
+        document.getElementById("valid_calendar").classList.remove("active");
+        document.getElementById("DayThird").classList.add("active");
+        for (var i = 0; i < 10; i++) {
+            const parts = labels3Days[i].split('-');
+            const date2 = parts.reverse().join('-');
+            DataUpdate(date2, 1, currency)
+        }
+        console.log(chartData.datasets[1].data)
+    }    
+    else {
+        chartData.labels = labelsDays;
+        chartOptions.scales.x.min = labelsDays[0];
+        chartOptions.scales.x.ticks.stepSize = 1;
+        document.getElementById("Hour").classList.remove("active");
+        document.getElementById("DayThird").classList.remove("active");
+        document.getElementById("Week").classList.remove("active");
+        document.getElementById("Month").classList.remove("active");
+        document.getElementById("valid_calendar").classList.remove("active");
+        document.getElementById("Day").classList.add("active");
+        for (var i = 0; i < 10; i++) {
+            const parts = labelsDays[i].split('-');
+            const date2 = parts.reverse().join('-');
+            DataUpdate(date2, 1, currency)
+        }
+        console.log(chartData.datasets[1].data)
+    }
 }
 
 var chart = new Chart(canvas, {
@@ -186,7 +257,22 @@ var chart = new Chart(canvas, {
     options: chartOptions
 });
 
+function BuildChart() {
+    chart.update();
+}
+
+function BurgerShow() {
+    var burgermenu = document.getElementById("burger_menu");
+    var burgerlog = document.getElementById("burger_logout");
+    burgermenu.classList.toggle("burgerActive")
+    burgerlog.classList.toggle("logActive")
+}
+
+
 document.getElementById("Day").addEventListener("click", DayPerDay);
 document.getElementById("DayThird").addEventListener("click", DayThirdDay);
 document.getElementById("calendar").addEventListener("click", SelectDate);
 document.getElementById("valid_calendar").addEventListener("click", SearchDate);
+document.getElementById("currency_choice_one").addEventListener("click", SelectCurrencyOne);
+document.getElementById("currency_choice_two").addEventListener("click", SelectCurrencyTwo);
+document.getElementById("burger_btn").addEventListener("click", BurgerShow);
